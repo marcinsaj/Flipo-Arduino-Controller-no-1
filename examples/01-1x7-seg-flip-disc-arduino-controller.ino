fc4cefab0b169bfcc4a8b2a3d60328a5bb62262e
Marcin Saj <marcin@nixietester.com>
2023-02-01 22:01:16 +0100

    Update 01-1x7-seg-flip-disc-arduino-controller.ino

--- examples/01-1x7-seg-flip-disc-arduino-controller.ino
+++ examples/01-1x7-seg-flip-disc-arduino-controller.ino
@@ -1,12 +1,12 @@
 /*----------------------------------------------------------------------------------*
  * Flip-disc display controller & 7-segment display                                 *
- * Example connection diagram: https://bit.ly/AC1-1x7SEG                            * 
+ * Example connection diagram: https://bit.ly/AC1-1x7SEG                            *
  *                                                                                  *
  * The MIT License                                                                  *
  * Marcin Saj 01 Feb 2023                                                           *
  * https://github.com/marcinsaj/FlipDisc                                            *
  *                                                                                  *
- * A dedicated controller is required to operate the display: https://bit.ly/AC1-FD *                    
+ * A dedicated controller is required: https://bit.ly/AC1-FD                        *
  *----------------------------------------------------------------------------------*/
 
 #include <FlipDisc.h>   /* https://github.com/marcinsaj/FlipDisc */ 
