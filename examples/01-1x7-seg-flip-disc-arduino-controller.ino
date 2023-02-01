9d7db25c79762e9bf60aa0f3b920d57c831de900
Marcin Saj <marcin@nixietester.com>
2023-02-01 21:39:52 +0100

    Update 01-1x7-seg-flip-disc-arduino-controller.ino

--- examples/01-1x7-seg-flip-disc-arduino-controller.ino
+++ examples/01-1x7-seg-flip-disc-arduino-controller.ino
@@ -1,1 +1,95 @@
+/*----------------------------------------------------------------------------------*
+ * Flip-disc display controller & 7-segment display                                 *
+ * Example connection diagram: https://bit.ly/AC1-1x7SEG                            * 
+ *                                                                                  *
+ * The MIT License                                                                  *
+ * Marcin Saj 01 Feb 2023                                                           *
+ * https://github.com/marcinsaj/FlipDisc                                            *
+ *                                                                                  *
+ * A dedicated controller is required to operate the display: https://bit.ly/AC1-FD *                    
+ *----------------------------------------------------------------------------------*/
 
+#include <FlipDisc.h>   // https://github.com/marcinsaj/FlipDisc 
+
+/* DO NOT CHANGE IT - pin declaration for the controller */
+#define EN_PIN  A7
+#define CH_PIN  A2 
+#define PL_PIN  A3
+
+void setup() 
+{
+  /* FlipDisc.Pin(); it is the most important function and first to call before everything else. 
+  The function is used to declare pin functions. Before starting the device, double check 
+  that the declarations and connection are correct. If the declarations of the control outputs 
+  is incorrect, the display may be physically damaged. */
+  FlipDisc.Pin(EN_PIN, CH_PIN, PL_PIN);
+  
+  /* FlipDisc.Init(display1, display2, ... display8); it is the second most important function. 
+  Initialization function for a series of displays. Up to 8 displays can be connected in series 
+  in any configuration. The function has 1 default argument and 7 optional arguments. 
+  The function also prepares SPI. Correct initialization requires code names of the serially 
+  connected displays:
+  - SEG - 7-segment display
+  - DOTS - 2x1 or 3x1 dot display
+  - FLIP3 - 1x3 display
+  - FLIP7 - 1x7 display   
+  Example function call for 8 modules: FlipDisc.Init(SEG,SEG,DOTS,SEG,SEG,DOTS,SEG,SEG); */
+
+  FlipDisc.Init(SEG);
+  delay(3000);
+}
+
+void loop() 
+{
+  /* The function is used to test all declared displays - turn on and off all displays */
+  FlipDisc.Test();
+  delay(3000);
+
+  /* The function is used to set the delay effect between flip discs. 
+  The default value without calling the function is 0. Can be called multiple times 
+  anywhere in the code. Recommended delay range: 0 - 100ms, max 255ms */
+  FlipDisc.Delay(50);
+
+  /* The function is used to turn on (set) all discs of all displays */
+  FlipDisc.All();
+  delay(3000);
+
+  /* The function is used to turn off (clear) all displays */
+  FlipDisc.Clear();
+  delay(3000);
+
+  /* An example of calling the function to display the number 5 
+  on the first 7-Segment display counting from the left */
+  FlipDisc.ToSeg(1, 5);
+  delay(3000);
+
+  /* Example function call assuming that we have declared one display 
+  and we want to display "F" - Fahrenheit symbol */ 
+  FlipDisc.Seg(FAH);
+  delay(3000);
+  /* This function allows you to display numbers and symbols: 0-9, "°","C" , "F", etc.
+  Code names for symbols:
+  - 8/ALL - Set all discs
+  - 10/CLR - Clear Display
+  - 11/DEG - "°" - Degree symbol
+  - 12/CEL - "C" - Celsius symbol
+  - 13/FAH - "F" - Fahrenheit symbol
+  - 14/HUM - "H" - Humidity symbol
+  - 15/PFH - "%" - Percent first half symbol
+  - 16/PSH - "%" - Percent second half symbol
+  - 17/UPD - "¯" - Upper dash symbol
+  - 18/MID - "-" - Middle dash symbol
+  - 19/BTD - "_" - Bottom dash symbol
+  - 20/ALD - All three dashes */ 
+
+  FlipDisc.Delay(0);
+  
+  /* Symbols can be displayed using their code names or numbers */
+  for(int symbolNumber = 0; symbolNumber <= 20; symbolNumber++)
+  {
+    FlipDisc.Seg(symbolNumber);
+    delay(1000);  
+  }
+  
+  delay(3000);
+}
