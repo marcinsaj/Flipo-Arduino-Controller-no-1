f38a033e50db6b75012ccb856a1c0d6c49f1e6bf
Marcin Saj <marcin@nixietester.com>
2023-02-11 21:55:40 +0100

    Update and rename 01-1x7-seg-flip-disc-arduino-controller.ino to 01-1x7seg-flip-disc-arduino-controller.ino

---
+++ examples/01-1x7seg-flip-disc-arduino-controller.ino
@@ -0,0 +1,10 @@
+/*----------------------------------------------------------------------------------*
+ * Flip-disc display controller & 7-segment display                                 *
+ * Example connection diagram: https://bit.ly/AC1-1x7SEG                            *
+ *                                                                                  *
+ * The MIT License                                                                  *
+ * Marcin Saj 01 Feb 2023                                                           *
+ * https://github.com/marcinsaj/FlipDisc                                            *
+ *                                                                                  *
+ * A dedicated controller is required: https://bit.ly/AC1-FD                        *
+ *----------------------------------------------------------------------------------*/
