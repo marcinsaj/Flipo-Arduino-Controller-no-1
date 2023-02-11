053fbd3bbd9a6a6e36c0c83e7773fad7c21eec62
Marcin Saj <marcin@nixietester.com>
2023-02-11 22:09:23 +0100

    Update 07-1x2x1-flip-disc-arduino-controller.ino

--- examples/07-1x2x1-flip-disc-arduino-controller.ino
+++ examples/07-1x2x1-flip-disc-arduino-controller.ino
@@ -8,3 +8,10 @@
  *                                                                                  *
  * A dedicated controller is required: https://bit.ly/AC1-FD                        *
  *----------------------------------------------------------------------------------*/
+
+#include <FlipDisc.h>   // https://github.com/marcinsaj/FlipDisc 
+
+// Pin declaration for Arduino Controller #1
+#define EN_PIN  A7  // Start & End SPI transfer data
+#define CH_PIN  A2  // Charging PSPS module - turn ON/OFF
+#define PL_PIN  A3  // Release the current pulse - turn ON/OFF
