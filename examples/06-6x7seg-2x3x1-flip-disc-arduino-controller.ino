f199791d609e99046ac5a5f6f5834e40dfaf7fee
Marcin Saj <marcin@nixietester.com>
2023-02-11 21:57:41 +0100

    Update and rename 06-6x7-seg-2x3dots-flip-disc-arduino-controller.ino to 06-6x7seg-2x3x1-flip-disc-arduino-controller.ino

---
+++ examples/06-6x7seg-2x3x1-flip-disc-arduino-controller.ino
@@ -0,0 +1,10 @@
+/*----------------------------------------------------------------------------------*
+ * Flip-disc display controller & 6 x 7-segment and 2 x 3x1 dot display             *
+ * Example connection diagram: https://bit.ly/AC1-6x7SEG-2x3x1DOTS                  *
+ *                                                                                  *
+ * The MIT License                                                                  *
+ * Marcin Saj 01 Feb 2023                                                           *
+ * https://github.com/marcinsaj/FlipDisc                                            *
+ *                                                                                  *
+ * A dedicated controller is required: https://bit.ly/AC1-FD                        *
+ *----------------------------------------------------------------------------------*/
